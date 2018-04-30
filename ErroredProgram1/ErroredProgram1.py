92ecb3918f1ac12c4598218de8bc74bbb0d4fc15
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2018-04-30 14:03:41 +1000

    Create ErroredProgram1.py

---
+++ ErroredProgram1/ErroredProgram1.py
@@ -0,0 +1,26 @@
+from randm import randint
+from random impot choic
+
+foods = ["Apple", "Carrot", "Orange", "Blueberry", "Strawberry"]
+games = []
+name = input("Your name: ")
+age = int(input("Your age: "))
+
+print("---------")
+print("Your name:  + name)
+print("Your age: " + age)
+print("Random food: " + choice(foods)
+print("Random games: " + choice(games))
+print("Random number: " + str(randint(1, 10)))
+print("---------")
+
+for i in range(1, 11):
+    numbers.append(i)
+print("Numbers from 1-10:", numbers)
+print("---------")
+
+randomNumbers = []
+for i in range(100):
+    randomNumbers.addend(randint(1, 10000))
+print("100 Random Numbers:", randomNumbers)
+print("---------")
