97e5a157a56caf7a71ec7b51ecbbfef840bf24c7
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2019-03-19 15:55:35 +1000

    Add no loop version of Program1

---
+++ Program1/Program1-NoLoops.py
@@ -0,0 +1,27 @@
+#
+# Explain what this program does
+#
+
+numberOne = int(input("Number? "))
+action = input("Action (+, -, / or *)? ")
+numberTwo = int(input("Number? "))
+if action == "+":
+    print()
+    print(numberOne + numberTwo)
+    print()
+elif action == "-":
+    print()
+    print(numberOne - numberTwo)
+    print()
+elif action == "/":
+    print()
+    print(numberOne / numberTwo)
+    print()
+elif action == "*":
+    print()
+    print(numberOne * numberTwo)
+    print()
+else:
+    print()
+    print("Invalid action! Supported actions are +, -, / and *!")
+    print()
